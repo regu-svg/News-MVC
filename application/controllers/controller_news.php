2af0fc9b847f4af6603e99081ba190e06bbe709e
regu-svg <57034605+regu-svg@users.noreply.github.com>
2019-12-19 18:40:12 +0400

    Update controller_news.php

--- application/controllers/controller_news.php
+++ application/controllers/controller_news.php
@@ -12,7 +12,8 @@ class controller_news extends Controller
     {
         $model = $this->model;
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            //проверка
+            //получение данных из формы
+            //отправка их в функцию
             $model->title = isset($_POST['title']) ? $_POST['title'] : null;
             $model->short_description = isset($_POST['short_description']) ? $_POST['short_description'] : null;
             $model->full_description = isset($_POST['full_description']) ? $_POST['full_description'] : null;
