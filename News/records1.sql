8079f1f5e3f42935b21d3b109d4f41c4a69c5bfb
regu-svg <57034605+regu-svg@users.noreply.github.com>
2019-12-20 20:40:07 +0400

    Add files via upload

--- News/records1.sql
+++ News/records1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Дек 20 2019 г., 08:50
+-- Время создания: Дек 20 2019 г., 19:38
 -- Версия сервера: 10.3.13-MariaDB-log
 -- Версия PHP: 7.1.32
 
@@ -34,7 +34,7 @@ CREATE TABLE `news` (
   `short_description` text NOT NULL,
   `full_description` text NOT NULL,
   `image_url` text NOT NULL,
-  `IP` int(11) NOT NULL
+  `IP` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -42,7 +42,7 @@ CREATE TABLE `news` (
 --
 
 INSERT INTO `news` (`ID`, `title`, `short_description`, `full_description`, `image_url`, `IP`) VALUES
-(4, 'ыпвраоп', 'ваврол', 'ыврлва', 'фыцпр', 0);
+(11, 'я не умею по рузке', 'Познание...', 'Я в своем познании настолько преисполнился, что я как будто бы уже сто триллионов миллиардов лет проживаю на триллионах и триллионах таких же планет, как эта Земля, мне этот мир абсолютно понятен, и я здесь ищу только одного - покоя, умиротворения и вот этой гармонии, от слияния с бесконечно вечным, от созерцания великого фрактального подобия и от вот этого замечательного всеединства существа, бесконечно вечного, куда ни посмотри, хоть вглубь - бесконечно малое, хоть ввысь - бесконечное большое, понимаешь?', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR9J-hsDwr1aVAXfMPOIpetBQUbjJntJisYgUNmqKs5gCbShoUj', '127.0.0.1');
 
 --
 -- Индексы сохранённых таблиц
@@ -62,7 +62,7 @@ ALTER TABLE `news`
 -- AUTO_INCREMENT для таблицы `news`
 --
 ALTER TABLE `news`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
